c960ccdc0a8562121d90b050defc2e937980076d
YANIN NOSUWAN (ญาณินท์ โนสุวรรณ) <80687176+liyizan@users.noreply.github.com>
2021-03-27 17:43:42 +0700

    lecture97accessdata

---
+++ lecture97/lecture97/AccessData.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture97
+{
+    class AccessData
+    {
+        public async static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS CharecterTable (uid INTEGER PRIMARY KEY, " +
+                    "Charecter_name NVARCHAR(50),"+
+                    "e_lement NVARCHAR(50),"+
+                    "phone_number NVARCHAR(50) NULL)";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                createTable.ExecuteReader();
+            }
+        }
+        public static void Adddata(string Charecter_name,string e_lement,string phone_number)
+        {
+            using(SqliteConnection db =
+                new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+                insertCommand.CommandText = "INSERT INTO CharecterTable VALUES (NULL,@Charecter_name,@e_lement,@phone_number)";
+                insertCommand.Parameters.AddWithValue("@Charecter_name",Charecter_name);
+                insertCommand.Parameters.AddWithValue("@e_lement", e_lement);
+                insertCommand.Parameters.AddWithValue("@phone_number", phone_number);
+                insertCommand.ExecuteReader();
+                db.Close();
+            }
+        }
+        
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT Charecter_name,e_lement,phone_number from CharecterTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                    entries.Add(query.GetString(1));
+                    entries.Add(query.GetString(2));
+                }
+                db.Close();
+            }
+            return entries;
+        }
+    }
+}
---
+++ lecture97/lecture97/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lecture97
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList arraylist;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            AccessData.InitializeDatabase();
+            arraylist = new ArrayList();
+            
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            AccessData.Adddata(txt_charactername.Text, txt_element.Text, txt_phone.Text);     
+            MessageBox.Show("add" +"\n Charecter name "+txt_charactername.Text +"\n element "+ txt_element.Text +"\n your number phone "+ txt_phone.Text );
+        }
+
+        private void btn_sHowall_Click(object sender, RoutedEventArgs e)
+        {
+
+            string charecterprofile = "";
+            foreach(string dAta in AccessData.GetData())
+            {
+                charecterprofile = charecterprofile +
+                    " charecter name " + dAta + "\n";
+            }
+            MessageBox.Show(charecterprofile);
+        }
+
+        
+    }
+}
